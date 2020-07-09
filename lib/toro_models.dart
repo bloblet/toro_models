2f067ff8752417183fc9bbb0a1750f7d8fcdb6c7
piecubed <40523150+piecubed@users.noreply.github.com>
2020-07-09 16:02:27 -0400

    add export

--- lib/toro_models.dart
+++ lib/toro_models.dart
@@ -3,3 +3,4 @@ library toro_models;
 export 'src/user.dart' show User, UserAdapter;
 export 'src/stock.dart' show Stock, StockAdapter;
 export 'src/portfolio_change_event.dart' show PortfolioChangeEvent, PortfolioChangeEventAdapter;
+export 'src/user_settings.dart' show UserSettings, UserSettingsAdapter;
