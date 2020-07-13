4db0c9acb92becdada60256f5dcbb45c4974ddcd
piecubed <40523150+piecubed@users.noreply.github.com>
2020-07-13 15:22:44 -0400

    Oops forgot this

--- lib/toro_models.dart
+++ lib/toro_models.dart
@@ -1,5 +1,6 @@
 library toro_models;
 
+export 'src/fetch_cache.dart' show FetchCache, FetchCacheAdapter;
 export 'src/user.dart' show User, UserAdapter;
 export 'src/stock.dart' show Stock, StockAdapter;
 export 'src/portfolio_change_event.dart' show PortfolioChangeEvent, PortfolioChangeEventAdapter;
